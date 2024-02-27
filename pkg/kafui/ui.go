eaa8a728fc388b9eccc52eac8a316f681a2ced8d
Benjamin Vollmer <vollmerbenjamin93@gmail.com>
2024-02-27 22:02:40 +0100

    Added search field

--- pkg/kafui/ui.go
+++ pkg/kafui/ui.go
@@ -10,16 +10,35 @@ import (
 func OpenUI(dataSource KafkaDataSource) {
 	// Create the application
 	app := tview.NewApplication()
+	table := tview.NewTable().SetBorders(false)
+	table.SetSelectable(true, false)
+	table.SetFixed(1, 1)
 
 	searchInput := tview.NewInputField().
 		SetLabel("Search: ").
 		SetFieldWidth(20)
+	searchText := ""
 
 	searchInput.SetDoneFunc(func(key tcell.Key) {
 		if key == tcell.KeyEnter {
-			searchText := searchInput.GetText()
-			// Handle search logic here
-			fmt.Println("Search text:", searchText)
+			searchText = searchInput.GetText()
+
+			if searchText == "context" {
+				table.Clear()
+				// Fetch context data from KafkaDataSource
+				contexts := fetchContexts(dataSource)
+				showContextsInTable(table, contexts)
+			}
+
+			if searchText == "topics" {
+				table.Clear()
+				topics := fetchTopics(dataSource)
+				showTopicsInTable(table, topics)
+			}
+			searchInput.SetText("")
+
+			app.SetFocus(table)
+
 		}
 	})
 
@@ -29,24 +48,9 @@ func OpenUI(dataSource KafkaDataSource) {
 		return
 	}*/
 
-	topics, err := dataSource.GetTopics()
-	if err != nil {
-		fmt.Println("Error reading topics")
-		return
-	}
+	topics := fetchTopics(dataSource)
 
-	table := tview.NewTable().SetBorders(false)
-	// Add header row
-	table.SetCell(0, 0, tview.NewTableCell("Topics").SetTextColor(tview.Styles.SecondaryTextColor))
-	table.SetSelectable(true, false)
-	table.SetFixed(1, 1)
-
-	// Add data rows
-	for i, topic := range topics {
-		cell := tview.NewTableCell(topic)
-		cell.SetExpansion(1)
-		table.SetCell(i+1, 0, cell)
-	}
+	showTopicsInTable(table, topics)
 
 	topFlex := tview.NewFlex().
 		AddItem(searchInput, 0, 1, true)
@@ -65,7 +69,54 @@ func OpenUI(dataSource KafkaDataSource) {
 	flex := tview.NewFlex().
 		AddItem(centralFlex, 0, 2, true)
 
+	// input
+
+	// Set the input capture to capture key events
+	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		// Check if the pressed key is Shift + :
+		if event.Key() == tcell.KeyRune && event.Modifiers() == tcell.ModShift && event.Rune() == ':' {
+			// Handle the Shift + : key combination
+			app.SetFocus(searchInput)
+			return nil // Return nil to indicate that the event has been handled
+		}
+		// Return the event to continue processing other key events
+		return event
+	})
+
 	if err := app.SetRoot(flex, true).EnableMouse(true).Run(); err != nil {
 		panic(err)
+	}
+}
+
+func showContextsInTable(table *tview.Table, contexts []string) {
+	table.SetCell(0, 0, tview.NewTableCell("Context").SetTextColor(tview.Styles.SecondaryTextColor))
+	for i, context := range contexts {
+		table.SetCell(i+1, 0, tview.NewTableCell(context))
+	}
+}
+
+func fetchContexts(dataSource KafkaDataSource) []string {
+	contexts, err := dataSource.GetContexts()
+	if err != nil {
+		fmt.Println("Error fetching contexts:", err)
+	}
+	return contexts
+}
+
+func fetchTopics(dataSource KafkaDataSource) []string {
+	topics, err := dataSource.GetTopics()
+	if err != nil {
+		fmt.Println("Error reading topics")
+	}
+	return topics
+}
+
+func showTopicsInTable(table *tview.Table, topics []string) {
+	table.SetCell(0, 0, tview.NewTableCell("Topics").SetTextColor(tview.Styles.SecondaryTextColor))
+
+	for i, topic := range topics {
+		cell := tview.NewTableCell(topic)
+		cell.SetExpansion(1)
+		table.SetCell(i+1, 0, cell)
 	}
 }
